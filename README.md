0704ab4ea492d1ef485003a3439c1fa4d0cbcf6e
James Lay <james@eventrentalsystems.com>
2025-11-25 16:47:30 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+ROADMAP:
+========
+
+1. encode canonical table in standard format
+2. encode to binary sequence
+3. try (en/de)coding the README (this file).
+4. make it DEFLATE compatible, so it can be used in zip, pdf, etc...
--- main.js
+++ main.js
@@ -1,36 +1,7 @@
 import assert from "node:assert";
 import Huffman from "./Huffman.js";
 
-let input = `
-Hello there, my name is James and I am writing a zip file generator.
-At some point, I'll also probably wanna write a zip file decompressor, but that's in the near future.
-
-Okay, so the first goal here is to compress this input string, then decompress it to 
-arrive at the same result.
-
-Technically, having done just that is successfully considered a huffman encoder.
-
-Which I've already done, actually.
-
-So, now the point is to canonically encode the huffman table so that we can
-encode and decode with the EXACT same tree.  Using the canonical representation
-may produce a different tree, but it will be guaranteed to have the exact same
-effect.  However, the canonical code table can be represented simply using a
-symbol-to-length mapping, which can be encoded in the smallest amount of space.
-This allows us to send all the info needed to deflate a stream with the minimal
-amount of space.
-
-And the good news is — that part is done!
-
-Here's what's left:
-
-1. use actual bit encodings instead of string representations.
-2. support unicode by operating on byte streams instead of JS strings.
-3. follow the deflate stream standard, encoding the huffman table into the payload as would be expected by PDF, pkzip, or gzip.
-
-`;
-
-//input = `aaaabcccddef`;
+let input = `hello world lllllllllllllllll hi llll that's a lot of "l"s. lllllllllll this should compress better the more I repeat byte, such as lllllllllll.`;
 
 const frequencies = Huffman.frequencies(input);
 //console.log("frequencies: ", frequencies);
@@ -56,12 +27,3 @@ console.log("encoded: %s", encodedBitStream.toString());
 const decoded = Huffman.decode(encodedBitStream, canonicalTree);
 console.log("decoded: %s", decoded);
  
-// // Actually, then the last step is to actually encode and decode from binary -
-// // then this becomes a legit util that can actually compress stuff.
-//         // make it more efficient! make it zip-compatible!
-//         // make it do actual byte-per-byte compression, btw - use Uint8Arrays.
-//         // run it from the browser! go cray-cray!
-// 
-// console.log("input length: %d", input.length);
-// console.log("encoded length: %d", encoded.length / 8);
-// console.log("compressed to %d% of original size", Math.floor(((encoded.length/8) / input.length) * 100));
